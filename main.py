f9b3b733d4f7c1e99419a49d9fffe8b59b375b9b
Pi <osoriopabl@gmail.com>
2020-02-23 13:07:40 -0500

    Pressing buttons

--- main.py
+++ main.py
@@ -1,34 +1,42 @@
-# import os
-# os.environ["KIVY_NO_CONSOLELOG"] = "0"
 from kivy.app import App
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.label import Label
-from kivy.uix.textinput import TextInput
+from kivy.uix.widget import Widget
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import ListProperty
 
-
-class LoginScreen(GridLayout):
+class RootWidget(BoxLayout):
 
     def __init__(self, **kwargs):
-        super(LoginScreen, self).__init__(**kwargs)
-        self.cols = 2
-        self.add_widget(Label(text='User Name'))
-        self.username = TextInput(multiline=False)
-        self.add_widget(self.username)
+        super().__init__(**kwargs)
+        self.add_widget(Button(text='btn 0'))
+        cb = CustomBtn()
+        cb.bind(pressed=self.btn_pressed)
+        self.add_widget(cb)
+        self.add_widget(Button(text='btn 2'))
 
-        self.add_widget(Label(text='password'))
-        self.password = TextInput(password=True, multiline=False)
-        self.add_widget(self.password)
-
-        self.add_widget(Label(text='Notes'))
-        self.notes = TextInput(multiline=True)
-        self.add_widget(self.notes)
+    def btn_pressed(self, instance, pos):
+        print ('pos: printed from root widget: {pos}'.format(pos=pos))
 
 
-class MyApp(App):
+class CustomBtn(Widget):
+
+    pressed = ListProperty([0, 0])
+    def on_touch_down(self, touch):
+        if self.collide_point(*touch.pos):
+            self.pressed = touch.pos
+            # we consumed the touch. return False here to propagate
+            # the touch further to the children.
+            return True
+        return super().on_touch_down(touch)
+
+    def on_pressed(self, instance, pos):
+        print ('pressed at {pos}'.format(pos=pos))
+
+class TestApp(App):
 
     def build(self):
-        return LoginScreen()
+        return RootWidget()
 
 
 if __name__ == '__main__':
-    MyApp().run()
+    TestApp().run()
